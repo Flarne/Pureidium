c71f1ece7a06419ff4819a4e19e8809d8529ad1e
Ulrik <ulrik.bergstrom@gmail.com>
2019-07-08 00:10:00 +0200

    Added Standard assets and made some movements on ship through scripts

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+	[Tooltip("In m / s -1")] [SerializeField] float xSpeed = 4f;
+
+    void Start()
+    {
+        
+    }
+	
+    void Update()
+    {
+		float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+		float xOffsetThisFrame = xThrow * xSpeed * Time.deltaTime;
+		print(xOffsetThisFrame);
+    }
+}
