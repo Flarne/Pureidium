a9a2744ee355241a2d1e2e273c75d7cd517fc7f0
Ulrik <ulrik.bergstrom@gmail.com>
2019-07-17 21:23:37 +0200

    Now we have music directly from game scene and made particles collisions and destroy enemy ship on hit

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour
 {
-
 	private void Awake()
 	{
-		DontDestroyOnLoad(gameObject);
+		int numMusicPlayers = FindObjectsOfType<MusicPlayer>().Length;
+		if (numMusicPlayers > 1)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			DontDestroyOnLoad(gameObject);
+		}
 	}
 }
