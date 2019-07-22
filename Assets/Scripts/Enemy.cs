11f15bc606e656e463e7c72604be6b8eb354f04b
Ulrik <ulrik.bergstrom@gmail.com>
2019-07-22 20:37:53 +0200

    Added some more enemies and changed some values on hitpoints

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,8 +7,8 @@ public class Enemy : MonoBehaviour
 {
 	[SerializeField] GameObject enemyDeathFX;
 	[SerializeField] Transform parent;
-
 	[SerializeField] int scorePerHit = 12;
+	[SerializeField] int hits = 10;
 
 	ScoreBoard scoreBoard;
 
@@ -26,7 +26,22 @@ public class Enemy : MonoBehaviour
 
 	private void OnParticleCollision(GameObject other)
 	{
+		ProcessHit();
+		if (hits <= 1)
+		{
+			KillEnemy();
+		}
+	}
+
+	private void ProcessHit()
+	{
 		scoreBoard.ScoreHit(scorePerHit);
+		hits = hits - 1;
+		// todo consider a particle effect on hit
+	}
+
+	private void KillEnemy()
+	{
 		GameObject fx = Instantiate(enemyDeathFX, transform.position, Quaternion.identity);
 		fx.transform.parent = parent;
 		Destroy(gameObject);
